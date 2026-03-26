a3fa39aae12089b3b07d55612c6abf719650c865
madushan321 <IT22182432@my.sliit.lk>
2026-03-26 23:46:56 +0530

    Add patient service with MVC structure

---
+++ patient-service/src/models/Patient.js
@@ -0,0 +1,6 @@
+let patients = [
+  { id: 1, name: "Kamal Perera",  age: 45, phone: "0771234567" },
+  { id: 2, name: "Nimali Silva",  age: 30, phone: "0769876543" }
+];
+
+module.exports = patients;
